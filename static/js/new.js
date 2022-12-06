9f9ddd7c58fc83607b7efcf359a0bd66b9a30f5f
harshadvali11 <harshadvali1431@gmail.com>
2022-12-06 18:49:37 +0530

    first commit

---
+++ static/js/new.js
@@ -0,0 +1,20 @@
+function click1() {
+    let div1=document.querySelector('#Box1');
+    div1.style.backgroundColor="#FB2576";
+    div1.innerText="HAI........."
+    div1.style.color="black"
+}
+function click2() {
+    let div2=document.querySelector('#Box2');
+    div2.style.backgroundColor="#540375"
+    div2.innerText="HELLO............";
+    div2.style.color="black"
+
+}
+function click3() {
+    let div3=document.querySelector('#Box3');
+    div3.style.backgroundColor="#557153"
+    div3.innerText="WELCOME.......!!!!!!";
+    div3.style.color="black"
+
+}
